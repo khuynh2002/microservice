c5a090e81424fcf14c09db5416d497ec5bc497c6
KEVINS\19717 <kevinhuynh200289@gmail.com>
2023-02-20 21:45:54 -0800

    fixed for partner

--- quote.py
+++ quote.py
@@ -1,3 +1,4 @@
+import random
 import json
 from flask import Flask, jsonify
 
@@ -11,6 +12,15 @@ with open("time-quotes.json", "r", encoding="utf-8") as f:
 # returns contents of json file as a GET request
 def get_quote():
     return jsonify(quotes)
+
+
+@app.route("/random.json", methods=['GET', 'POST'])
+def get_random_quote():
+    if not quotes:
+        return "Error. No quotes.", 404
+    random_index = random.randint(0, len(quotes["quotes"]) - 1)
+    random_quote = quotes["quotes"][random_index]
+    return jsonify(random_quote) 
 
 
 if __name__ == "__main__":
